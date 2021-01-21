a49b8a6d45ebc3323fbb34863e510535f4353ffa
fanshijian <fanshij@163.com>
2021-01-21 10:30:44 +0800

    D1

--- TestCHGAdapterOC/AppDelegate.m
+++ TestCHGAdapterOC/AppDelegate.m
@@ -16,7 +16,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    // C1
+    // C1 D1
     return YES;
 }
 
